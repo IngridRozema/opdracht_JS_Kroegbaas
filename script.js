b351135d91018bab2b9f6984282bcbe1e55faeb4
Ingrid Rozema <67099368+IngridRozema@users.noreply.github.com>
2021-07-07 11:36:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const age = 43;
+if (age > 18) {
+    greeting = "Welkom! We wensen je een fijne avond!"
+}
+if (age < 18) {
+    greeting = "Helaas kunnen we je niet binnenlaten. We wensen je toch een fijne avond!"
+}
+console.log(greeting)
+
+const isFemale = true;
+if (isFemale == true) {
+    greeting = "welkom op de Ladiesnight!"
+}
+if (isFemale == false) {
+    greeting = "Vanavond zijn alleen dames welkom!"
+}
+console.log(greeting)
+
+const driverStatus = 'bob';
+if (driverStatus == 'bob') {
+    greeting = "Rijd voorzichtig & wel thuis!"
+}
+else { 
+    greeting = "Het is niet verantwoord dat u gaat rijden."
+}
+console.log(greeting)
